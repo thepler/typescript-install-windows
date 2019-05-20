496fff58ab79033c5a753517bfaeb794555df913
Todd Hepler <Todd.Hepler@ehi.com>
2019-05-20 16:11:23 -0500

    this seems to work

--- install-typescript.sh
+++ install-typescript.sh
@@ -5,16 +5,11 @@ set -e
 
 wget --quiet https://nodejs.org/dist/v10.15.3/node-v10.15.3-win-x64.zip
 unzip -q node-v10.15.3-win-x64.zip
-chmod +x node-v10.15.3-win-x64/npm node-v10.15.3-win-x64/node.exe
-chmod +x node-v10.15.3-win-x64/node_modules/npm/bin/*
-export PATH="$PATH:$(pwd)/node-v10.15.3-win-x64/node_modules/npm/bin"
-export PATH="$PATH:$(pwd)/node-v10.15.3-win-x64"
-which npm
-which node
-npm install npm -g
-which npm
-npm install node -g
-which node
-npm install typescript -g
-which tsc
+cd node-v10.15.3-win-x64
+chmod +x npm* node*
+./npm install typescript -g
+./npm install ts-node -g
+set +x
+echo "add this to your PATH:"
+pwd
 
