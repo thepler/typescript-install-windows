02754c5398b570908dce302f047ed27d4d352a86
Todd Hepler <Todd.Hepler@ehi.com>
2019-05-20 16:42:11 -0500

    add npx

--- install-typescript.sh
+++ install-typescript.sh
@@ -6,7 +6,7 @@ set -e
 wget --quiet https://nodejs.org/dist/v10.15.3/node-v10.15.3-win-x64.zip
 unzip -q node-v10.15.3-win-x64.zip
 cd node-v10.15.3-win-x64
-chmod +x npm* node*
+chmod +x npm* node* npx*
 ./npm install typescript -g
 ./npm install ts-node -g
 set +x
